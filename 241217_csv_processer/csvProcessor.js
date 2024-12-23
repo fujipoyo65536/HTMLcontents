63fcec502b42b0674636c41d821cf84cce29f84f
fujipoyo65536 <mfuji810@gmail.com>
2024-12-24 01:02:13 +0900

    index.htmlのファイル選択機能を改善し、複数ファイル選択を可能にしました。また、読み込みオプションを整理し、プレビュー表示機能を追加しました。処理と出力のステップを新たに追加し、今後の機能についての説明を含めました。

--- 241217_csv_processer/csvProcessor.js
+++ 241217_csv_processer/csvProcessor.js
@@ -5,261 +5,287 @@
 // DOM読み込み後のイベント設定
 document.addEventListener('DOMContentLoaded', function() {
 
-  // #csvDataInputが変更されたらプレビューを更新
-  document.getElementById('csvDataInput').addEventListener('input', function(){
-    updatePreview();
-  });
+	// #loadingOptions内のinputに変更があったらプレビューを更新
+	const loadingOptions = document.getElementById('loadingOptions');
+	loadingOptions.addEventListener('input', function(){
+		updatePreview();
+	});
+	// #csvDataInputに変更があったらプレビューを更新
+	const csvDataInput = document.getElementById('csvDataInput');
+	csvDataInput.addEventListener('input', function(){
+		updatePreview();
+	});
+	// #previewCharLimitInputに変更があったらプレビューを更新
+	const previewCharLimitInput = document.getElementById('previewCharLimitInput');
+	previewCharLimitInput.addEventListener('input', function(){
+		updatePreview();
+	});
 
-  document.getElementById('delimiterInput').addEventListener('input', function(){
-    updatePreview();
-  });
-  document.getElementById('lineBreakInput').addEventListener('input', function(){
-    updatePreview();
-  });
-  document.getElementById('skipRowNumberInput').addEventListener('input', function(){
-    updatePreview();
-  });
-
-  document.getElementById('isHeaderCheckbox').addEventListener('change', function(){
-    updatePreview();
-  });
+	// ファイルが入力されたら、いったん1ファイル目だけを読み込む
+	const fileInput = document.getElementById('csvFileInput');
+	fileInput.addEventListener('change', function(){
+		const file = fileInput.files[0];
+		const reader = new FileReader();
+		reader.onload = function(){
+			document.getElementById('csvDataInput').value = reader.result.slice(0, 20000);
+			updatePreview();
+		};
+		reader.readAsText(file);
+	});
 
 
-  //デバッグ用
-  document.getElementById('csvDataInput').value = '1,2,3\n4,5,6\n7,"8",9\ntest1,"te\nst2"\nabc,def,ghi,jkl,mno\n\npqr,stu,vwx,yz';
-  updatePreview();
+	//デバッグ用
+	document.getElementById('csvDataInput').value = '1,2,3\n4,5,6\n7,"8",9\n"te""s""t1","te\nst2","te,st3"\nabc,def,ghi,jkl,mno\n\npqr,stu,vwx,yz\n';
+	updatePreview();
 });
 
 function updatePreview(){
-  const inputText = document.getElementById('csvDataInput').value;
-  const previewTableElement = document.getElementById('csvPreviewTable');
+	const limit = document.getElementById('previewCharLimitInput').value;
+	const inputText = document.getElementById('csvDataInput').value.slice(0, limit);
+	const previewTableElement = document.getElementById('csvPreviewTable');
 
-  // プレビューを更新
-  previewTableElement.innerHTML = '';
+	// プレビューを更新
+	previewTableElement.innerHTML = '';
 
-  const csvArray = csvTextToArray(inputText);
-  if(csvArray.length == 0 || (csvArray.length == 1 && csvArray[0] == null)){
-    previewTableElement.innerHTML = '<td>(データ無し)</td>';
-    return;
-  }
-  //0行目がnullでないならヘッダー行を表示
-  if(csvArray[0] !== null){
-    let headerRow = document.createElement('tr');
-    csvArray[0].forEach(function(cell){
-      let headerCell = document.createElement('th');
-      headerCell.textContent = cell;
-      headerRow.appendChild(headerCell);
-    });
-    previewTableElement.appendChild(headerRow);
-  }
-  // データ行を表示
-  for(let i = 1; i < csvArray.length; i++){
-    let row = document.createElement('tr');
-    csvArray[i].forEach(function(cell){
-      let cellElement = document.createElement('td');
-      cellElement.textContent = cell;
-      row.appendChild(cellElement);
-    });
-    previewTableElement.appendChild(row);
-  }
-  
+	const csvArray = csvTextToArray(inputText);
+	if(csvArray.length == 0 || (csvArray.length == 1 && csvArray[0] == null)){
+		previewTableElement.innerHTML = '<td>(データ無し)</td>';
+		return;
+	}
+	//0行目がnullでないならヘッダー行を表示
+	if(csvArray[0] !== null){
+		let headerRow = document.createElement('tr');
+		csvArray[0].forEach(function(cell){
+			let headerCell = document.createElement('th');
+			headerCell.textContent = cell;
+			headerRow.appendChild(headerCell);
+		});
+		previewTableElement.appendChild(headerRow);
+	}
+	// データ行を表示
+	for(let i = 1; i < csvArray.length; i++){
+		let row = document.createElement('tr');
+		csvArray[i].forEach(function(cell){
+			let cellElement = document.createElement('td');
+			cellElement.textContent = cell;
+			row.appendChild(cellElement);
+		});
+		previewTableElement.appendChild(row);
+	}
+	
 }
 
 function csvTextToArray(csvText){
-  const delimiter = document.getElementById('delimiterInput').value || ',';
-  const lineBreakSelect = document.getElementById('lineBreakInput').value || 'ALL';
-  let lineBreak
-  switch(lineBreakSelect){
-    case 'ALL':
-      lineBreak = /\r\n|\r|\n/;
-      break;
-    case 'LF':
-      lineBreak = /\n/;
-      break;
-    case 'CR':
-      lineBreak = /\r/;
-      break;
-    case 'CRLF':
-      lineBreak = /\r\n/;
-      break;
-    default:
-      lineBreak = /\n/;
-      break;
-  }
-  const skipRowNumber = document.getElementById('skipRowNumberInput').value || 0;
-  const isHeader = document.getElementById('isHeaderCheckbox').checked || false;
-  const wrapper = document.getElementById('wrapperInput').value || '"';
+	const delimiter = document.getElementById('delimiterInput').value || ',';
+	const lineBreakSelect = document.getElementById('lineBreakInput').value || 'ALL';
+	let lineBreak
+	switch(lineBreakSelect){
+		case 'ALL':
+			lineBreak = /[\r\n|\r|\n]/;
+			break;
+		case 'LF':
+			lineBreak = /\n/;
+			break;
+		case 'CR':
+			lineBreak = /\r/;
+			break;
+		case 'CRLF':
+			lineBreak = /\r\n/;
+			break;
+		default:
+			lineBreak = /\n/;
+			break;
+	}
+	const skipRowNumber = document.getElementById('skipRowNumberInput').value || 0;
+	const skipEmptyRow = document.getElementById('skipEmptyRowCheckbox').checked || false;
+	const ignoreLastLineBreak = document.getElementById('ignoreLastLineBreakCheckbox').checked || false;
+	const isHeader = document.getElementById('isHeaderCheckbox').checked || false;
+	const wrapper = document.getElementById('wrapperInput').value || '"';
 
-  const rows = csvText.split(lineBreak);
-  
-  // 先頭行を読み飛ばして捨てる
-  for(let i = 0; i < skipRowNumber; i++){
-    rows.shift();
-  }
+	//入力末尾の改行を消す
+	if(ignoreLastLineBreak){
+		csvText = csvText.replace(/[\r\n|\r|\n]$/, '');
+	}
 
-  // 以下、データの複雑さによって分岐する(工事中)
+	const rows = csvText.split(lineBreak);
+	// 先頭行を読み飛ばして捨てる
+	for(let i = 0; i < skipRowNumber; i++){
+		rows.shift();
+	}
 
-// レベル2メソッド
-  // {
-  //   let result = [];
-  //   // ヘッダー行があるならresult[0]に格納
-  //   if(isHeader){
-  //     result[0] =rows.shift().split(delimiter);
-  //   }else{
-  //     result[0] = null;
-  //   }
-  //   // それ以外の行を格納
-  //   for(let i = 0; i < rows.length; i++){
-  //     const rowData = rows[i].split(delimiter);
-  //     let row = [];
-  //     for(let j = 0; j < rowData.length; j++){
-  //       const cellData = rowData[j];
-  //       let cell = cellData;
-  //       // 両側の空白を削除
-  //       cell = cell.trim();
-  //       // 両側の囲み文字を削除(囲み文字に両側を囲まれている場合のみ)
-  //       cell = cell.replace(new RegExp(`^${wrapper}(.*)${wrapper}$`), '$1');
-  //       // 囲み文字の中の囲み文字をエスケープ
-  //       cell = cell.replace(new RegExp(wrapper + wrapper, 'g'), wrapper);
-  //       // 結果に追加
-  //       row.push(cell);
-  //     }
-  //     result.push(row);
-  //   }
-  //   console.log(result);
-  // }
 
-　//レベル3メソッド
-  {
-    // 時間計測のテスト
+	// 処理モードの決定(データの複雑さによる)
+	// 1:最初に改行で分割し、その後デリミタで分割していいもの
+	// 2:最初に改行で分割するが、先頭から各セルを判断する必要があるもの
+	// 3:すべて1文字ずつ処理するもの
 
-    // const start = performance.now();
-    // const testText = csvText
-    // const regExp = new RegExp(`(?=[${delimiter}${lineBreak}${wrapper}\r\n])|(?<=[${delimiter}${lineBreak}${wrapper}\r\n])`);
-    // let outputText = ""
-    // for(let i = 0; i < 10000; i++){
-    //   outputText = testText.split(regExp);
-    // }
-    // const end = performance.now();
-    // console.log(outputText);
-    // console.log(`レベル3メソッド: ${end - start}ms`);
+	const isUseWrapper = document.getElementById('isUseWrapperCheckbox').checked || false;
+	const isUseDelimiterInWrapper = document.getElementById('isUseDelimiterInWrapperCheckbox').checked || false;
+	const isUseWrapperInWrapper = document.getElementById('isUseWrapperInWrapperCheckbox').checked || false;
+	const isUseLineBreakInWrapper = document.getElementById('isUseLineBreakInWrapperCheckbox').checked || false;
 
-    // const start2 = performance.now();
-    // const testText2 = csvText
-    // let outputText2 = ""
-    // for(let i = 0; i < 10000; i++){
-    //   outputText2 = testText2.split("");
-    // }
-    // const end2 = performance.now();
-    // console.log(outputText2);
-    // console.log(`単純なsplit: ${end2 - start2}ms`);
 
-    let splitText;
-    if(skipRowNumber > 0){
-      splitText = rows.join("\n")
-    }else{
-      splitText = csvText.split("");
-    }
-    let result=[];
-    if(!isHeader){
-      result[0] = null;
-    }
-    let cell = "";
-    let row = [];
-    // ステータス関係
-    let afterDelimiter = false;
-    let afterLineBreak = true;
-    let inWrapper = false;
-    let afterWrapperCharactor = false; //ラッパー内でラッパーが1つ出てきたとき
-    
-    for(let i = 0; i < splitText.length; i++){
-      const char = splitText[i];
-      const lineBreakMatch = (char.match(lineBreak)||[null])[0];
-      // ここから1文字ずつ処理していく
-      if(inWrapper){
-        switch(char){
-          case wrapper:
-            if(afterWrapperCharactor){
-              afterWrapperCharactor = false;
-              cell += char;
-            }else{
-              afterWrapperCharactor = true;
-            }
-            break;
-          case delimiter:
-            if(afterWrapperCharactor){
-              inWrapper = false;
-            }else{
-              cell += char;
-            }
-            afterWrapperCharactor = false;
-            break;
-          case lineBreakMatch:
-            if(afterWrapperCharactor){
-              inWrapper = false;
-            }else{
-              cell += char;
-            }
-            afterWrapperCharactor = false;
-            break;
-          default:
-            cell += char;
-            afterWrapperCharactor = false;
-            break;
-        }
-      }
-      if(!inWrapper){
-        switch(char){
-          case wrapper:
-            // ラッパーの外でラッパー文字が出てきたらおかしい。が淡々と追加
-            if(afterDelimiter || afterLineBreak){
-              inWrapper = true;
-            }else{
-              cell += char;
-            }
-            break;
-          case delimiter:
-            // デリミターが出てきたらセルを追加
-            row.push(cell);
-            cell = "";
-            afterDelimiter = true;
-            break;
-          case lineBreakMatch:
-            if(!afterLineBreak){ // 連続する改行は無視
-              // 改行が出てきたらセルを追加して行を追加
-              row.push(cell);
-              result.push(row);
-              row = [];
-              cell = "";
-              afterLineBreak = true;
-            }
-            break;
-          default:
-            cell += char;
-            break;
-        }
-        if(char != delimiter){
-          afterDelimiter = false
-        };
-        if(char != lineBreakMatch){
-          afterLineBreak = false
-        };
-        afterWrapperCharactor = false;
-      }//if(inWrapper)
-    }//1文字ずつ
-    
-    // 最後のセルを追加
-    if(cell != ""){
-      row.push(cell);
-    }
-    if(row.length > 0){
-      result.push(row);
-    }
+	let readingMode
+	if(!isUseWrapper){
+		readingMode = 1;
+	}else if(isUseLineBreakInWrapper){
+		readingMode = 3;
+	}else if(isUseDelimiterInWrapper){
+		readingMode = 2;
+	}else{
+		readingMode = 1;
+	}
 
-    console.log(result);
-    return result;
-  }
-  // return result;
+	document.getElementById('readingModeView').textContent = `[debug]読み取りモード:${readingMode}`;//★
 
- 
+	let result=[];
+	switch(readingMode){
+		case 1:
+			{
+				if(!isHeader){
+					result[0] = null;
+				}
+				// それ以外の行を格納
+				for(let i = 0; i < rows.length; i++){
+					const rowData = rows[i].split(delimiter);
+					if(skipEmptyRow && rowData.length == 1 && rowData[0] == ""){
+						//行が空の場合はスキップ
+						continue;
+					}
+					let row = [];
+					for(let j = 0; j < rowData.length; j++){
+						const cellData = rowData[j];
+						let cell = cellData;
+						// 両側の空白を削除
+						cell = cell.trim();
+						if(isUseWrapper){
+							// 両側の囲み文字を削除(囲み文字に両側を囲まれている場合のみ)
+							// 両側が囲み文字かを確認
+							if(cell.startsWith(wrapper) && cell.endsWith(wrapper)){
+								// 両側を削除
+								cell = cell.slice(1, -1);
+								if(isUseWrapperInWrapper){
+									// 囲み文字の中の囲み文字をエスケープ
+									cell = cell.replace(new RegExp(wrapper + wrapper, 'g'), wrapper);
+								}
+							}								
+						}
+						// 結果に追加
+						row.push(cell);
+					}
+					result.push(row);
+				}
+			}
+			break;
+
+		case 2:
+			// 今作っていないのでレベル3で処理
+		case 3:
+			{
+				let splitText;
+				if(skipRowNumber > 0){
+					splitText = rows.join("\n")
+				}else{
+					splitText = csvText.split("");
+				}
+				if(!isHeader){
+					result[0] = null;
+				}
+				let cell = "";
+				let row = [];
+				// ステータス関係
+				let afterDelimiter = false;
+				let afterLineBreak = true;
+				let inWrapper = false;
+				let afterWrapperCharactor = false; //ラッパー内でラッパーが1つ出てきたとき
+				
+				for(let i = 0; i < splitText.length; i++){
+					const char = splitText[i];
+					const lineBreakMatch = (char.match(lineBreak)||[null])[0];
+					// ここから1文字ずつ処理していく
+					if(inWrapper){
+						switch(char){
+							case wrapper:
+								if(afterWrapperCharactor){
+									afterWrapperCharactor = false;
+									cell += char;
+								}else{
+									afterWrapperCharactor = true;
+								}
+								break;
+							case delimiter:
+								if(afterWrapperCharactor){
+									inWrapper = false;
+								}else{
+									cell += char;
+								}
+								afterWrapperCharactor = false;
+								break;
+							case lineBreakMatch:
+								if(afterWrapperCharactor){
+									inWrapper = false;
+								}else{
+									cell += char;
+								}
+								afterWrapperCharactor = false;
+								break;
+							default:
+								cell += char;
+								afterWrapperCharactor = false;
+								break;
+						}
+					}
+					if(!inWrapper){
+						switch(char){
+							case wrapper:
+								// ラッパーの外でラッパー文字が出てきたらおかしい。が淡々と追加
+								if(afterDelimiter || afterLineBreak){
+									inWrapper = true;
+								}else{
+									cell += char;
+								}
+								break;
+							case delimiter:
+								// デリミターが出てきたらセルを追加
+								row.push(cell);
+								cell = "";
+								afterDelimiter = true;
+								break;
+							case lineBreakMatch:
+								if(!afterLineBreak || !skipEmptyRow){ // 連続する改行は無視
+									// 改行が出てきたらセルを追加して行を追加
+									row.push(cell);
+									result.push(row);
+									row = [];
+									cell = "";
+									afterLineBreak = true;
+								}
+								break;
+							default:
+								cell += char;
+								break;
+						}
+						if(char != delimiter){
+							afterDelimiter = false
+						};
+						if(char != lineBreakMatch){
+							afterLineBreak = false
+						};
+						afterWrapperCharactor = false;
+					}//if(inWrapper)
+				}//1文字ずつ
+				
+				// 最後のセルを追加
+				// if(cell != ""){
+					row.push(cell);
+				// }
+				// if(row.length > 0){
+					result.push(row);
+				// }
+
+			}
+			break;
+		}//switch
+		console.log(result);
+		return result; 
 }
